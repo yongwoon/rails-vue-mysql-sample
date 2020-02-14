81fc4a79abe4d5f963b43fe57af2c9eb2991a9d4
yongwoon kim <yonyon.japan@gmail.com>
2020-02-14 15:24:55 +0900

    post#index 画面実装

---
+++ app/javascript/views/posts/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th colspan="2">POST LIST</th>
+      </tr>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>created_at</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="post in posts" :key="post.id">
+        <td>{{ post.id }}</td>
+        <td>{{ post.title }}</td>
+        <td>{{ post.created_at }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "PostIndex",
+  props: {
+    posts: Array
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
