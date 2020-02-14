5c4d97b2813b4df77d64e2a98617f39462eba989
yongwoon kim <yonyon.japan@gmail.com>
2020-02-14 10:07:18 +0900

    post model migrate

--- db/migrate/20200214005926_create_posts.rb
+++ db/migrate/20200214005926_create_posts.rb
@@ -1,8 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
+      t.references :user, foreign_key: true
+      t.string :title, null: false
+      t.text :body, null: false
 
       t.timestamps
+
+      t.index [:user_id, :created_at]
     end
   end
 end
