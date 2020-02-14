c0be7a228863fcafd3fc5e871d1a8b2d43358832
yongwoon kim <yonyon.japan@gmail.com>
2020-02-14 17:42:55 +0900

    不要なファイル削除

--- app/javascript/packs/posts/index.js
+++ app/javascript/packs/posts/index.js
@@ -5,9 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const element = document.getElementById("posts-index-view");
   const data = JSON.parse(element.getAttribute("data"));
 
-  console.log("test");
-  console.log(data.posts);
-
   new Vue({
     el: element,
     render: h => h(PostsIndexView, { props: { posts: data.posts } })
